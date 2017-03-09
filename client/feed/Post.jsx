d5a2b12feea4f6358c0d918d2be121961d67efe5
Tarkan Corak <tarkan@corak.org>
2017-03-09 15:34:16 +0000

    added link to user profile page

--- client/feed/Post.jsx
+++ client/feed/Post.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Post = ({post}) => (
   <div className="Post">
     <img src={post.imgUrl} alt=""/>
     <div className="PostHeader">
-      <span className="Author">{`Created by @${post.handle} on ${post.createdAt}`}</span>
+      <span className="Author">
+        <Link to={`/profile/${post.handle}`}>
+          {`Created by @${post.handle} on ${post.createdAt}`}
+        </Link>
+      </span>
       <span className="Likes">{`Likes (${post.likes.length})`}</span>
     </div>
     <p>{post.description}</p>
